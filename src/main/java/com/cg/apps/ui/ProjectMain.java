fdaf639fe4f7ea463c52ab7b640491fa15e4464a
MahimDadhich <dadhich.mahim97@gmail.com>
2020-10-30 10:03:32 +0530

    UI class

--- src/main/java/com/cg/apps/ui/ProjectMain.java
+++ src/main/java/com/cg/apps/ui/ProjectMain.java
@@ -1,21 +1,21 @@
-package com.cg.main;
+package com.cg.apps.ui;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.cg.customerms.JavaConfiguration;
-import com.cg.exception.CustomerNotFoundException;
-import com.cg.main.ProjectUI;
+import com.cg.apps.ui.ProjectUI;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
-public class ProjectMain 
-{
-	public static void main(String[] args) 
-	{
-			
-			AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-			context.register(JavaConfiguration.class);
-			context.refresh();
-			ProjectUI projectUi = context.getBean(ProjectUI.class);
-			projectUi.runUI();
-			context.close();
-		}
+@SpringBootApplication
+public class ProjectMain {
+
+    public static void main(String args[]) 
+    {
+        ConfigurableApplicationContext context = SpringApplication.run(ProjectMain.class, args);
+        ProjectUI ui = context.getBean(ProjectUI.class);
+        ui.runUI();
+        context.close();
+    }
 }
--- src/main/java/com/cg/apps/ui/ProjectUI.java
+++ src/main/java/com/cg/apps/ui/ProjectUI.java
@@ -2,6 +2,7 @@ package com.cg.apps.ui;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.cg.apps.entities.*;
+import com.cg.apps.exception.CustomerNotFoundException;
 import com.cg.apps.service.*;
 
 @Component
@@ -12,21 +13,27 @@ public class ProjectUI
 
 		public void runUI()
 		{
-			Customer customerObj1 = addObj(1L, "Harshit");
-			Customer customerObj2 = addObj(2L, "Shivam");
-			Customer customerObj3= addObj(3L, "Riya");
+			try
+			{
+			Customer customerObj1 = addObj("Harshit");
+			Customer customerObj2 = addObj("Shivam");
+			Customer customerObj3= addObj("Riya");
 			
 			System.out.println("Customer 1: "+customerObj1+"\nCustomer 2: "+customerObj2+"\nCustomer 3: "+customerObj3);
 
 			Long idUpdate = customerObj1.getId();	
 			customerObj1 = updateObj(idUpdate, "Naman");
 			System.out.println("Customer 1: "+customerObj1);
-			
+			}
+			catch(CustomerNotFoundException e)
+			{
+				System.out.println(e.getMessage());
+			}
 		}
 		
-		public Customer addObj(Long id, String name)
+		public Customer addObj(String name)
 		{
-			Customer customer = new Customer(id, name);
+			Customer customer = new Customer(name);
 			customer = customerService.register(customer);
 			return customer;
 		}
